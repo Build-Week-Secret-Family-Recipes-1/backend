d56b648928adb81e6a525b397bfeee76322f9daf
Anthony Ware <aware91coding@gmail.com>
2020-08-19 21:06:10 -0500

    start some data

--- data/migrations/20200819204545_creater-users-table.js
+++ data/migrations/20200819204545_creater-users-table.js
@@ -2,11 +2,18 @@ exports.up = function(knex) {
   return knex.schema
     .createTable("users", tbl => {
       tbl.increments();
-      tbl.string("fname", 128).notNullable();
-      tbl.string("lname", 128).notNullable();
-      tbl.varchar("email", 128).notNullable().unique();
-      tbl.varchar("username", 128).notNullable().unique();
-      tbl.varchar("password", 128).notNullable();
+      tbl.string("fname", 128)
+        .notNullable();
+      tbl.string("lname", 128)
+        .notNullable();
+      tbl.varchar("email", 128)
+        .notNullable()
+        .unique();
+      tbl.varchar("username", 128)
+        .notNullable()
+        .unique();
+      tbl.varchar("password", 128)
+        .notNullable();
     })
 };
 
--- data/migrations/20200819204605_create-recipes-table.js
+++ data/migrations/20200819204605_create-recipes-table.js
@@ -2,8 +2,15 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('recipes', tbl => {
-      
+      tbl.increments();
+      tbl.string('name', 128)
+        .notNullable();
+      tbl.string('description', 255)
+        .notNullable();
+      tbl.string('source', 128)
+        .notNullable();
     })
+    
 };
 
 exports.down = function(knex) {

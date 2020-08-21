4261a7fdad20f7abeac48eea52a91d4b2745ccef
Anthony Ware <aware91coding@gmail.com>
2020-08-20 20:00:58 -0500

    heroku test

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const pgConnection = process.env.DATABASE_URL || "postgres://postgres:1790@localhost:5432/unit4db";
+const pgConnection = process.env.DATABASE_URL || "postgres://oyemyqtbmvqzbt:486a932b8d91b828d30a96a4c2a984ca659377fe144367afa4ff6036939e48b0@ec2-54-236-146-234.compute-1.amazonaws.com:5432/d1eqhpt4gjkmfp";
 
 module.exports = {
 

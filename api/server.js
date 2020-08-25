815a609fcd904fa27810afcafb351b719a317a81
Anthony Ware <aware91coding@gmail.com>
2020-08-24 20:31:16 -0500

    fix cors

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
-const cors = require("helmet");
+const cors = require("cors");
 const morgan = require("morgan");
 
 const authRouter = require("../auth/authRouter.js");
